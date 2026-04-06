1bb9ca6a0e70a0475369e9bf492161ac1d5f8fd6
kencrimsonIT <tongkien930@gmail.com>
2026-04-06 10:52:55 +0700

    fix: xóa trùng lặp products

--- frontend/src/data/products.js
+++ frontend/src/data/products.js
@@ -107,8 +107,6 @@ const normalizedSimpleProducts = simpleProducts.map((p, index) => ({
 }));
 export const products = [...detailedProducts, ...normalizedSimpleProducts];
 
-export const products = PRODUCTS
-
 export function getProducts() {
     return products;
 }
